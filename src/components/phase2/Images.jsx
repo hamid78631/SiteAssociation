8987e552c62f853fb3b00a67225cea2902b25706
hamid78631 <hamidoumamoudou125@icloud.com>
2025-07-29 17:09:00 +0100

    modif

--- src/components/phase2/Images.jsx
+++ src/components/phase2/Images.jsx
@@ -9,20 +9,20 @@ import 'swiper/css/navigation';
 import { EffectFade, Pagination, Navigation } from 'swiper/modules';
 
 import slide_image_1 from "../../assets/forum1.jpeg";
-import slide_image_2 from "../../assets/forum2.jpeg";
+// import slide_image_2 from "../../assets/forum2.jpeg";
 import slide_image_3 from "../../assets/forum3.jpeg";
-import slide_image_4 from "../../assets/forum4.jpeg";
-import slide_image_5 from "../../assets/forum6.jpeg";
-import slide_image_6 from "../../assets/forum7.jpeg";
+// import slide_image_4 from "../../assets/forum4.jpeg";
+import slide_image_6 from "../../assets/forum6.jpeg";
+// import slide_image_7 from "../../assets/forum7.jpeg";
 
 function Images() {
   // Logs pour vérifier les chemins d’images
   console.log("slide_image_1:", slide_image_1);
-  console.log("slide_image_2:", slide_image_2);
+  // console.log("slide_image_2:", slide_image_2);
   console.log("slide_image_3:", slide_image_3);
-  console.log("slide_image_4:", slide_image_4);
-  console.log("slide_image_5:", slide_image_5);
+  // console.log("slide_image_4:", slide_image_4);
   console.log("slide_image_6:", slide_image_6);
+  // console.log("slide_image_7:", slide_image_7);
 
   return (
     <div className="container">
@@ -48,21 +48,21 @@ function Images() {
         <SwiperSlide>
           <img src={slide_image_1} alt="slide_image" />
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <img src={slide_image_2} alt="slide_image" />
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <img src={slide_image_3} alt="slide_image" />
         </SwiperSlide>
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <img src={slide_image_4} alt="slide_image" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={slide_image_5} alt="slide_image" />
-        </SwiperSlide>
+        </SwiperSlide> */}
         <SwiperSlide>
           <img src={slide_image_6} alt="slide_image" />
         </SwiperSlide>
+        {/* <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide> */}
 
         {/* Navigation + Pagination */}
         <div className="slider-controler">
